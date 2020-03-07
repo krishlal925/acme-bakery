d4efeff99ca1d9b38420f67957332846ece356f6
Neel <krishlal925@gmail.com>
2020-03-07 11:38:22 -0800

    created chef form component

---
+++ src/chefform.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ChefForm = ()=>{
+
+  return(
+  <div>
+    <form className= 'form'>
+      Enter New Chef's name:
+      <input ></input>
+      <button>Create</button>
+    </form>
+  </div>
+  );
+}
+
+export default ChefForm;
--- src/index.js
+++ src/index.js
@@ -2,32 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Chefs from './chefslist';
-import Recipes from './recipeslist'
+import Recipes from './recipeslist';
+import ChefForm from './chefform';
+
 const {useState, useEffect} = React;
 
-// const Recipes = ({recipes, destroyRecipe}) => {
-//   console.log("recipes: ", recipes);
-//   return (
-//     <div>
-// <div>Recipes({recipes.length})</div>
-//       <ul>
-//         {
-//           recipes.map(recipe =>{
-//            return( <li key = {recipe.id}>{recipe.name}
-//             <button onClick= {()=> destroyRecipe(recipe)}>x</button>
-//            </li>)
-//           })
-//         }
-//       </ul>
-//     </div>
-//   )
-// }
 
 const App = () => {
   const [chefs, setChefs] = useState([]);
   const [recipes, setRecipes] = useState([]);
   const [ error, setError ] = useState('');
-  console.log("1: ", chefs);
 
   const destroyChef = async({id})=>{
     try{
@@ -58,17 +42,18 @@ const App = () => {
     Promise.all([axios.get('/api/chefs'), axios.get('/api/recipes')])
       .then(responses => responses.map(response => response.data))
       .then(results => {
-        console.log("2: ", results);
         setChefs(results[0]);
         setRecipes(results[1]);
     })
   }, [])
-  console.log("3: ", chefs);
+
+
   return (
     <div>
       <div>My App... </div>
       <div className='container'>
         <div className= 'column1' >
+          <ChefForm />
           <Chefs chefs = {chefs} destroyChef = {destroyChef}/>
         </div>
         <div className= 'column2'>
